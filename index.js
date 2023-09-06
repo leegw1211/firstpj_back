b62069309830534fc52f84c925eb7ec5e6eeaa49
leegw1211 <leegw1211@pusan.ac.kr>
2023-09-06 17:11:34 +0900

    secure은 되는데 왜 samesite는 안돼?

--- index.js
+++ index.js
@@ -64,9 +64,9 @@ app.post('/login', (req, res) => {
             connection.query(`insert into sessionid (userid, string, expire_date) \
             values ('${results[0].uid}', '${sessionId}', DATE_ADD(NOW(),INTERVAL + 1 day))`, (error2, results2) => {
                 res.cookie('sessionId', sessionId, {
-                    httpOnly: false, // 클라이언트에서 쿠키 조작 방지
-                    secure: true, // HTTPS에서만 전송
-                    sameSite: 'none'
+                    sameSite: 'none',
+                    httpOnly: true, // 클라이언트에서 쿠키 조작 방지
+                    secure: true // HTTPS에서만 전송
                 });
                 res.send(results);
             })
