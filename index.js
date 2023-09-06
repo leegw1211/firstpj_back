5b13e7275844e5124994be9fe135b15baaf47ff5
leegw1211 <leegw1211@pusan.ac.kr>
2023-09-06 11:08:30 +0900

    쿠키 값 문제 > credentials옵션 수정

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const fs = require('fs');
 const crypto = require('crypto');
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+    origin: '*', // 출처 허용 옵션
+    credential: 'true' // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+}));
 app.use(express.json());
 app.use(express.urlencoded( {extended : false } ));
 const maria = require('mysql'); // 호환됌!!!
