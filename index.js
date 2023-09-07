c3daa541d3cf2f5e67239e9e1baac501015eb038
leegw1211 <leegw1211@pusan.ac.kr>
2023-09-07 19:34:24 +0900

    로그아웃을 위해 새 api 추가

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post('/login', (req, res) => {
             values ('${results[0].uid}', '${sessionId}', DATE_ADD(NOW(),INTERVAL + 1 day))`, (error2, results2) => {
                 res.cookie('sessionId', sessionId, {
                     sameSite: 'none',
-                    httpOnly: false, // 클라이언트에서 쿠키 조작 방지
+                    httpOnly: true, // 클라이언트에서 쿠키 조작 방지
                     secure: true // HTTPS에서만 전송
                 });
                 res.send(results);
@@ -74,8 +74,17 @@ app.post('/login', (req, res) => {
     });
 });
 
+app.post('/logout', (req, res) => {
+    res.cookie('sessionId', '', {
+        sameSite: 'none',
+        expires: new Date(0),
+        httpOnly: true, // 클라이언트에서 쿠키 조작 방지
+        secure: true // HTTPS에서만 전송
+    });
+    res.send(results);
+});
+
 app.post('/usercheck', (req, res) => {
-    console.log("123");
     connection.query(`select * from sessionid where string='${req.cookies.sessionId}';`, (error, results) => {
         if (results.length == 0) res.send(results);
         else {
